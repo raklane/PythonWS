7a4f0e22177c9286ae633e5fe1819f566c0e8ea5
Rakesh Kumar <rakesh.kumar123@t-mobile.com>
2020-07-20 14:31:13 -0500

    initialfolderspush

---
+++ myprogram.py
@@ -0,0 +1,5 @@
+from mymainpackage import some_main_script
+from mymainpackage.subpackage import mysubscript
+
+some_main_script.report_main()
+mysubscript.sub_report()
---
+++ pyfiles/__name__ and __main__/one.py
@@ -0,0 +1,11 @@
+#one.py
+
+def func():
+	print('func() in one.py')
+
+print('Top level in one.py')
+
+if __name__ == '__main__':
+	print('One.py is being run direclty.')
+else:
+	print('One.py has been imported.')
